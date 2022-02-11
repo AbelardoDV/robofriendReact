f511ebc3a78dc62671c8be8279fcc187658a30fc
Rodolfo <rodolfo.diaz@nokia.com>
2022-02-10 19:36:07 -0500

    Updates

---
+++ precache-manifest.e3001ecddf41557cb9210432119aec4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d01f754dfa5d5421adcaef495ef253c",
+    "url": "/robofriendReact/index.html"
+  },
+  {
+    "revision": "e6ae48733b00bad9a5d3",
+    "url": "/robofriendReact/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "950063ee1972cc56ee93",
+    "url": "/robofriendReact/static/css/main.ed776d1f.chunk.css"
+  },
+  {
+    "revision": "e6ae48733b00bad9a5d3",
+    "url": "/robofriendReact/static/js/2.28589087.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robofriendReact/static/js/2.28589087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd201b8c407754fbcd02",
+    "url": "/robofriendReact/static/js/3.ea4ca9ab.chunk.js"
+  },
+  {
+    "revision": "950063ee1972cc56ee93",
+    "url": "/robofriendReact/static/js/main.33c35991.chunk.js"
+  },
+  {
+    "revision": "ddd2af6e5ffdcbdb196f",
+    "url": "/robofriendReact/static/js/runtime-main.23c53baa.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendReact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
